7ed27b9c57de2ba4abe795d6a87f0729ce6e66e8
nriviere-gg-qa <nriviere-gg-qa@proton.me>
2024-01-31 15:23:50 +0100

    Update notif.py

--- notif.py
+++ notif.py
@@ -5,3 +5,6 @@ openai.api_key = "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"
 
 
 apikey = 'NRRA-b51a0643a54d967dff9057c355213abaaf5d604209'
+
+
+NRRA-a58b898371a7a89dd8f565d7afe9dbaa2979a99d0f
